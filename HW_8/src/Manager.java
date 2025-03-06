5c7111fc723e3b35bfb7959bf715178060ec245d
JannaPaliakova <Janna.Paliakova@itechart-group.com>
2025-03-06 10:04:18 +0300

    HW8

--- HW_8/src/Manager.java
+++ HW_8/src/Manager.java
@@ -1,3 +1,10 @@
+//Задача №3
+//Необходимо создать класс Manager с полями: имя, возраст, пол, ЗП в день
+// и количество подчиненных. Класс должен иметь метод -
+// getSalary(Month[]monthArray), метод возвращает зарплату за те месяцы
+// которые были переданы в качестве аргумента.
+//К рассчитанной ЗП должно прибавляться по 1% за каждого подчиненного.
+
 public class Manager {
     public String name;          // Имя сотрудника
     public int age;              // Возраст
@@ -5,7 +12,7 @@ public class Manager {
     public double salaryPerDay;  // Зарплата в день
     public int noOfSubordinates;
 
-    // Метод для вычисления зарплаты за переданные месяцы
+    // Метод для вычисления зарплаты за переданные месяцы.
     public double getSalary(Month[] monthArray) {
         double totalSalary = 0;
 
